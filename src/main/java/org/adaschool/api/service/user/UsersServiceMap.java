8e7cb3a7ee3756b2d908b66fbdbe3dc398f14535
Yesid MORA <yesid.mora@mail.escuelaing.edu.co>
2023-02-19 16:42:57 -0500

    Implementation UserController and UserService

--- src/main/java/org/adaschool/api/service/user/UsersServiceMap.java
+++ src/main/java/org/adaschool/api/service/user/UsersServiceMap.java
@@ -1,36 +1,45 @@
 package org.adaschool.api.service.user;
 
+import org.adaschool.api.exception.UserNotFoundException;
 import org.adaschool.api.repository.user.User;
+import org.adaschool.api.repository.user.UserDto;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UsersServiceMap implements UsersService {
 
+    Map<String, User> users = new HashMap<>();
+
     @Override
     public User save(User user) {
-        return null;
+        if(findById(user.getId()).isEmpty()){
+            user.setId(String.valueOf(users.size()));
+        }
+        users.put(user.getId(), user);
+        return users.get(user.getId());
     }
 
     @Override
     public Optional<User> findById(String id) {
-        return Optional.empty();
+        return Optional.ofNullable(users.get(id));
     }
 
     @Override
     public List<User> all() {
-        return null;
+        return new ArrayList<>(users.values());
     }
 
     @Override
     public void deleteById(String id) {
-
+        users.remove(id);
     }
 
     @Override
     public User update(User user, String userId) {
-        return null;
+        User userToUpdate = users.get(userId);
+        userToUpdate.update(new UserDto(user.getName(), user.getLastName(), user.getEmail()));
+        return userToUpdate;
     }
 }
